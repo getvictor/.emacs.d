ec93e159b1b49b3a8433b87e62645d81056b28ca
getvictor <victor.lyuboslavsky@gmail.com>
2015-11-21 07:58:19 -0500

    Added YAML mode

--- init.el
+++ init.el
@@ -143,6 +143,10 @@
 ;; setup files ending in ".rdl" to open in ruby-mode
 (add-to-list 'auto-mode-alist '("\\.rdl\\'" . ruby-mode))
 
+;; yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
 ;; verilog mode
 (setq verilog-auto-endcomments nil)
 (setq verilog-indent-level              2
